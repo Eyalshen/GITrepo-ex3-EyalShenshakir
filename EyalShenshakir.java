adaf33c2f7166cbea8bc6a5453c0167f2352e181
Eyal Shenshakir <eialsh@mta.ac.il>
2022-11-20 00:38:44 +0200

    Changing Eyal SHenshakir content for new build trigger by jenkins

--- EyalShenshakir.java
+++ EyalShenshakir.java
@@ -1,7 +1,7 @@
-public class Hello{
+public class EyalShenshakir{
 	public static void main(String[] arg){
 		for(int i=1; i<=10;i++){
-		System.out.println("Eyal Shenshakir"+i);
+		System.out.println("Second job " + i);
 		}
 	}
 }
